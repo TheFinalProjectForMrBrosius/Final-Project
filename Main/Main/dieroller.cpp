67ea4e004f539b780bff77d12fc94383eeea339b
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2018-05-03 13:33:05 -0500

    update

--- Main/Main/dieroller.cpp
+++ Main/Main/dieroller.cpp
@@ -1,8 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdlib>
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	int rand();
+	double rollOfDieOne, rollOfDieTwo, totalDiceScore;
+	string userChoice;
+	
+	do{
+		cout << "Would you like to roll a dice? >>>" << endl;
+		cin >> userChoice;
+		rollOfDieOne = rand() % 6 + 1;
+		rollOfDieTwo = rand() % 6 + 1;
+		//random numbers for the the two dice
+		if (userChoice == "Yes" || userChoice == "yes")
+		{
+			totalDiceScore = rollOfDieOne + rollOfDieTwo;
+			cout << "The number of the first die is  " << rollOfDieOne << endl;
+			cout << "The number of the second die is  " << rollOfDieTwo << endl;
+			cout << "Your total score is " << totalDiceScore << endl;
+		}
+		//If the user says no the the question on the top
+		else if (userChoice == "NO" || userChoice == "no")
+		{
+			cout << "Ok have a nice day" << endl;
+		}
+	} while (userChoice != "yes");
+
+
 	system("Pause");
 	return 0;
 }
