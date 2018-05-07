dccc6a318d20b18b005168c87f232c47544c6ba0
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2018-05-07 12:46:08 -0500

    update

--- Main/Main/CasinoGames.h
+++ Main/Main/CasinoGames.h
@@ -1,10 +1,12 @@
-#include <iostream>
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
-using namespace std;
 
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+int casinoGame1()
 {
 	int userAmount;
 	int secret;
@@ -33,6 +35,76 @@ int main()
 	{
 		cout << "Your bet is to HIGH kick it down a little" << endl;
 	}
+	system("pause");
+	return 0;
+}
+
+
+
+int diceGame()
+{
+	int rand();
+	double rollOfDieOne, rollOfDieTwo, totalDiceScore;
+	string userChoice;
+
+	do{
+		cout << "|--------------------------------------|" << endl;
+		cout << "|Welcome To The BEST CASINO EVER!!!!!!!|" << endl;
+		cout << "|--------------------------------------|" << endl;
+		cout << "Would you like to roll a dice? >>>" << endl;
+		cin >> userChoice;
+		rollOfDieOne = rand() % 6 + 1;
+		rollOfDieTwo = rand() % 6 + 1;
+		//random numbers for the the two dice
+		if (userChoice == "Yes" || userChoice == "yes")
+		{
+			totalDiceScore = rollOfDieOne + rollOfDieTwo;
+			cout << "The number of the first die is  " << rollOfDieOne << endl;
+			cout << "The number of the second die is  " << rollOfDieTwo << endl;
+			cout << "Your total score is " << totalDiceScore << endl;
+		}
+		//If the user says no the the question on the top
+		else if (userChoice == "NO" || userChoice == "no")
+		{
+			cout << "Ok have a nice day then" << endl;
+		}
+	} while (userChoice != "yes");
+
+
+	system("Pause");
+	return 0;
+}
+
+
+int numberGuessing()
+{
+	int numberChoice;
+	int secret;
+	int count = 0;
+
+	srand(time(NULL));
+
+	secret = rand() % 100 + 1;
+	do{
+		cout << "|--------------------------------------|" << endl;
+		cout << "|Welcome To The BEST CASINO EVER!!!!!!!|" << endl;
+		cout << "|--------------------------------------|" << endl;
+		cout << "Please pick a number between 1 - 10" << endl;
+		cin >> numberChoice;
+		if (numberChoice > secret)
+		{
+			cout << "The number you chose is to high!, guess agian loser" << endl;
+			count = count + 1;
+			//count++//
+		}
+		else if (numberChoice < secret)
+		{
+			cout << "The number you have chose is to low!, guess again loser" << endl;
+			count = count + 1;
+		}
+	} while (numberChoice != secret);
+	cout << "Congrats! " << endl;
+
 	system("pause");
 	return 0;
 }
